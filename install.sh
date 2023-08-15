d5a1ec61c81502cf03eef58622e6490719c4f79a
Nils <nils865@users.noreply.github.com>
2023-08-15 17:30:47 +0200

    Added tmux

--- install.sh
+++ install.sh
@@ -1,13 +1,19 @@
 git clone https://github.com/nils865/.dotfiles $HOME/.dotfiles
 
 rm -rf $HOME/.zshrc
-rm -rf $HOME/.config/nvim
 rm -rf $HOME/.gitconfig
+rm -rf $HOME/.config/nvim
+rm -rf $HOME/.config/tmux
 
 ln -s $HOME/.dotfiles/zsh/.zshrc $HOME/.zshrc
-ln -s $HOME/.dotfiles/nvim $HOME/.config/nvim
 ln -s $HOME/.dotfiles/git_config/.gitconfig $HOME/.gitconfig
+ln -s $HOME/.dotfiles/nvim $HOME/.config/nvim
+ln -s $HOME/.dotfiles/tmux $HOME/.config/tmux
 
+# neovim plugins
 git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+
+# tmux plugins
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
---
+++ tmux/keybinds.md
@@ -0,0 +1,9 @@
+# All Custom TMUX Keybinds
+
+- Leader / Prefix `Ctrl + Space`
+- Split `Ctrl + Alt + Arrows`
+- Move Between Panes `Ctrl + Arrows`
+- Resize Panes `Alt + Arrows`
+- Reload Config `<prefix> r`
+- Kill Session `Alt + q`
+- Kill Pane `Ctrl + q`
